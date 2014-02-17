fd134adc4045b51fd6a19070a9211d1c6590b93b
Muhammad S. Maromi <msmaromi@gmail.com>
2014-02-17 23:31:11 +0700

    tambah komen penjelasan

--- frame.h
+++ frame.h
@@ -4,12 +4,13 @@
 #include <graphics.h>
 
 typedef struct{
-int xDos;
-int yDos;
+int xDos;   //x yang dihitung dari layar DOS (positif: kekanan)
+int yDos;   //y yang dihitung dari layar DOS (positif: kebawah)
 } Frame;
 
-Frame makeFrame(int x, int y);
-int getXDos(Frame *frame, int xFrame);
-int getYDos(Frame *frame, int yframe);
+Frame makeFrame(int x, int y);  //x,y dihitung terhadap DOS
+int getXDos(Frame *frame, int xFrame);  //xFrame adalah x yang dihitung terhadap koordinat xDos, yDos (positif: kekanan)
+int getYDos(Frame *frame, int yframe);  //yFrame adalah y yang dihitung terhadap koordinat xDos, yDos (positif: keatas)
+
 
 #endif
